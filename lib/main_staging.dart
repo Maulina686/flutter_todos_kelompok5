dc4a78e90952e4784ba38331c73d12c40a7b2a03
Maulina686 <maulina686@gmail.com>
2026-05-13 11:54:34 +0700

    entrypoint

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_todos/bootstrap.dart';
 import 'package:local_storage_todos_api/local_storage_todos_api.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  var SharedPreferences;
+ 
   final todosApi = LocalStorageTodosApi(
     plugin: await SharedPreferences.getInstance(),
   );
